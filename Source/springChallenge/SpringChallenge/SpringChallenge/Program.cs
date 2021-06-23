618396bb952d7a04816a0d3f9e38df6db239779c
baka <bakayarusama@gmail.com>
2021-06-23 22:12:54 +0200

    first step complete tree when tree ismine

--- Source/springChallenge/SpringChallenge/SpringChallenge/Program.cs
+++ Source/springChallenge/SpringChallenge/SpringChallenge/Program.cs
@@ -14,19 +14,10 @@ class Player
     static void Main(string[] args)
     {
         string[] inputs;
-        int numberOfCells = int.Parse(Console.ReadLine()); // 37
-        for (int i = 0; i < numberOfCells; i++)
-        {
-            inputs = Console.ReadLine().Split(' ');
-            int index = int.Parse(inputs[0]); // 0 is the center cell, the next cells spiral outwards
-            int richness = int.Parse(inputs[1]); // 0 if the cell is unusable, 1-3 for usable cells
-            int neigh0 = int.Parse(inputs[2]); // the index of the neighbouring cell for each direction
-            int neigh1 = int.Parse(inputs[3]);
-            int neigh2 = int.Parse(inputs[4]);
-            int neigh3 = int.Parse(inputs[5]);
-            int neigh4 = int.Parse(inputs[6]);
-            int neigh5 = int.Parse(inputs[7]);
-        }
+
+        var cells = GetCells();
+
+        cells.ForEach(Console.Error.WriteLine);
 
         // game loop
         while (true)
@@ -40,27 +31,109 @@ class Player
             int oppSun = int.Parse(inputs[0]); // opponent's sun points
             int oppScore = int.Parse(inputs[1]); // opponent's score
             bool oppIsWaiting = inputs[2] != "0"; // whether your opponent is asleep until the next day
-            int numberOfTrees = int.Parse(Console.ReadLine()); // the current amount of trees
-            for (int i = 0; i < numberOfTrees; i++)
-            {
-                inputs = Console.ReadLine().Split(' ');
-                int cellIndex = int.Parse(inputs[0]); // location of this tree
-                int size = int.Parse(inputs[1]); // size of this tree: 0-3
-                bool isMine = inputs[2] != "0"; // 1 if this is your tree
-                bool isDormant = inputs[3] != "0"; // 1 if this tree is dormant
-            }
+
+            var trees = GetTrees(cells);
+
+            trees.ForEach(Console.Error.WriteLine);
+
             int numberOfPossibleActions = int.Parse(Console.ReadLine()); // all legal actions
             for (int i = 0; i < numberOfPossibleActions; i++)
             {
                 string possibleAction = Console.ReadLine(); // try printing something from here to start with
             }
 
-            // Write an action using Console.WriteLine()
-            // To debug: Console.Error.WriteLine("Debug messages...");
-
-
             // GROW cellIdx | SEED sourceIdx targetIdx | COMPLETE cellIdx | WAIT <message>
-            Console.WriteLine("WAIT");
+            var tree = trees.FirstOrDefault(t => t.IsMine);
+
+            if(tree != null)
+                Console.WriteLine($"COMPLETE {tree.Cell.Index}");
+            else
+                Console.WriteLine("WAIT");
+        }
+
+        static List<Cell> GetCells()
+        {
+            var cells = new List<Cell>();
+            string[] inputs;
+            int numberOfCells = int.Parse(Console.ReadLine()); // 37
+            for (int i = 0; i < numberOfCells; i++)
+            {
+                inputs = Console.ReadLine().Split(' ');
+
+                cells.Add(new Cell
+                {
+                    Index = int.Parse(inputs[0]), // 0 is the center cell, the next cells spiral outwards
+                    Richness = int.Parse(inputs[1]), // 0 if the cell is unusable, 1-3 for usable cells
+                    NeighbouringCell0 = int.Parse(inputs[2]), // the index of the neighbouring cell for each direction
+                    NeighbouringCell1 = int.Parse(inputs[3]),
+                    NeighbouringCell2 = int.Parse(inputs[4]),
+                    NeighbouringCell3 = int.Parse(inputs[5]),
+                    NeighbouringCell4 = int.Parse(inputs[6]),
+                    NeighbouringCell5 = int.Parse(inputs[7])
+                });
+            }
+            return cells;
+        }
+    }
+
+    private static List<Tree> GetTrees(List<Cell> cells)
+    {
+        var trees = new List<Tree>();
+        int numberOfTrees = int.Parse(Console.ReadLine()); // the current amount of trees
+        for (int i = 0; i < numberOfTrees; i++)
+        {
+            var inputs = Console.ReadLine().Split(' ');
+            int cellIndex = int.Parse(inputs[0]); // location of this tree
+            trees.Add(new Tree
+            {
+                Cell = cells.Find(c => c.Index == cellIndex),
+                Size = int.Parse(inputs[1]), // size of this tree: 0-3
+                IsMine = inputs[2] != "0", // 1 if this is your tree
+                IsDormant = inputs[3] != "0" // 1 if this tree is dormant
+            });
+        }
+
+        return trees;
+    }
+
+    class Tree
+    {
+        public Cell Cell { get; set; }
+        public int Size { get; set; }
+        public bool IsMine { get; set; }
+        public bool IsDormant { get; set; }
+
+        public override string ToString()
+        {
+            return $"Cell {Cell} \r" +
+                $"Size {Size} " +
+                $"IsMine {IsMine} " +
+                $"IsDormant {IsDormant}";
+        }
+    }
+
+    class Cell
+    {
+        public int Index { get; set; }
+        public int Richness { get; set; }
+        public int NeighbouringCell0 { get; set; }
+        public int NeighbouringCell1 { get; set; }
+        public int NeighbouringCell2 { get; set; }
+        public int NeighbouringCell3 { get; set; }
+        public int NeighbouringCell4 { get; set; }
+        public int NeighbouringCell5 { get; set; }
+
+        public override string ToString()
+        {
+            return $"Index {Index} " +
+                $"Richness {Richness} " +
+                $"NC0 {NeighbouringCell0} " +
+                $"NC1 {NeighbouringCell1} " +
+                $"NC2 {NeighbouringCell2} " +
+                $"NC3 {NeighbouringCell3} " +
+                $"NC4 {NeighbouringCell4} " +
+                $"NC5 {NeighbouringCell5} ";
+
         }
     }
 }
